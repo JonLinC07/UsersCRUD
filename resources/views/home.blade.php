07c4f3cfc69b2efd6d42424126a5bc9f7f82c378
oscar jonathan misael lin coronado <ojmlc93@gmail.com>
2020-06-27 13:37:53 -0700

    [ADD] Se agrego funcionalidad de login, obtención de registros de la base de datos, y estructuras de control

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,18 +4,22 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+            <!-- HERE put add new user button -->
+            <a href=""> <i class="fas fa-plus-circle"></i> Add new user</a>
+
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">List of users</div>
+                
+                @forelse ($users as $user)
+                    <div class="card-body">
+                        <i class="fas fa-user"></i> <p>{{ $user->name }}</p> <p>{{ $user->email }}</p> <p>{{ $user->phone }}</p>
+                    </div>
+                @empty
+                    <div class="card-body">
+                        <i class="fas fa-user-times"></i> We have no users yet
+                    </div>
+                @endforelse
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
             </div>
         </div>
     </div>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,3 @@
+<div class="header">
+    <h1>USERS CRUD</h1>
+</div>
